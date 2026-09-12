0ddacca7f9eb58c2cd9cf6efc264868bf755ff5a
Harshil-18-Byte <harshilwaghela189@gmail.com>
2026-09-12 18:05:15 +0530

    Configure Gradle library plugin and repository fallbacks for modern AGP compatibility

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,6 +5,20 @@ allprojects {
     }
 }
 
+// Emulate legacy jcenter() method on RepositoryHandler for legacy Flutter plugins
+try {
+    val metaClass = groovy.lang.GroovySystem.getMetaClassRegistry()
+        .getMetaClass(org.gradle.api.artifacts.dsl.RepositoryHandler::class.java)
+    if (metaClass is groovy.lang.ExpandoMetaClass) {
+        metaClass.registerInstanceMethod("jcenter", object : groovy.lang.Closure<Any>(this) {
+            fun doCall(): Any {
+                val handler = delegate as org.gradle.api.artifacts.dsl.RepositoryHandler
+                return handler.mavenCentral()
+            }
+        })
+    }
+} catch (_: Throwable) {}
+
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
